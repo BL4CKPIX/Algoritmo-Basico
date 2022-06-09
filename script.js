cbe49a91ec91d06f52bd923ad196bf4abe5c9310
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-06-09 14:53:13 -0300

    Algoritmo Complet

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = ('JØRDAN SANTØS')
+let number = 19
+let ano = 2022
+
+console.log (nome)
+console.log (number)
+console.log (ano)
+console.log (ano - number)
+console.log (ano + number)
+console.log ('Estou mexendo no console.log')
+
+let quartaFeira = false
+console.log ('quartaFeira')
+console.log (typeof quartaFeira)
